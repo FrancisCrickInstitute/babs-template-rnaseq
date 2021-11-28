f923c3a11a1b709d305decfdd761a15bd9d832b7
Gavin Kelly <gavin.kelly@crick.ac.uk>
2021-11-28 15:01:13 +0000

    Initialised by DESdemonA
     "DESdemonA": { "Package": "DESdemonA", "Version": "0.20.1", "Source": "Local", "Hash": "3a115026408a841e6dfef0827258a314" },

---
+++ my_proj/differential/00_init.r
@@ -0,0 +1,90 @@
+defaults <- list(
+  nfcore      = "../results",     ## directory that contains nfcore output
+  metadata    = "inst/extdata/metadata.xlsx",    ## Path to  spreadsheet containing covariates
+  file_col    = c("filename", "sample", "sample_ID", "Sample.limsid"),    ## Column in that spreadsheet that identifies the basename of count file for each sample
+  name_col    = c("name", "label", "sample", "filename"),    ## Column in that spreadsheet that identifies the basename of count file for each sample
+  counts      = "../results/star_rsem",      ## directory that contains the quantified gene counts
+  org_package = "", ## NULL will derive org.*.eg.db from nfcore info, otherwise specifcy string identifying annotation package of relevant species.
+  spec_prefix = ""
+)
+
+
+library(readxl)
+library(tidyverse)
+library(tximport)
+library(DESeq2)
+library(DESdemonA)
+library(R.utils)
+
+
+args <- R.utils::cmdArgs(args=defaults)
+
+sample_sheet <- readxl::read_excel(args$metadata, sheet=1)
+file_col <- args$file_col[args$file_col %in% names(sample_sheet)][1]
+if (is.na(file_col)) {
+  stop("Metadata has none of: ", paste(args$file_col, collapse=", "))
+}
+name_col <- args$name_col[args$name_col %in% names(sample_sheet)][1]
+if (is.na(name_col)) {
+  stop("Metadata has none of: ", paste(args$name_col, collapse=", "))
+}
+
+
+sample_sheet[sapply(sample_sheet, is.character)] <- lapply(sample_sheet[sapply(sample_sheet, is.character)], 
+                                                          as.factor)
+
+tx_path <-   file.path(
+  args$counts,
+  paste0(as.character(sample_sheet[[file_col]]), ".genes.results")
+)
+
+names(tx_path) <- as.character(sample_sheet[[name_col]])
+
+txi <- tximport(tx_path, type="rsem")
+txi$length[txi$length==0] <- 1
+
+dds <- DESeqDataSetFromTximport(txi, sample_sheet, ~ 1) # ok to have fixed trivial design - it'll get swapped out
+ind <- rowSums(counts(dds)!=0) > 0
+dds <- dds[ind,]
+
+txi[c("abundance", "counts", "length")] <- map(txi[c("abundance", "counts", "length")], ~ .[ind,])
+
+
+if (args$org_package=="") {
+  run_info <- readLines(file.path(args$nfcore, "pipeline_info", "pipeline_report.txt"))
+  gtf_file <- sub(".*/", "", grep(" - gtf:", run_info, value=TRUE))
+  organism <- list(binomial=strsplit(gtf_file, ".", fixed=TRUE)[[1]][1])
+  organism <- list(
+    genus   = sub("_.*", "", organism$binomial),
+    species = sub(".*_", "", organism$binomial)
+  )
+  organism$Gs <- paste0(toupper(substr(organism$genus, 1, 1)),
+                       tolower(substr(organism$species, 1, 1)))
+  organism$org <-  unique(grep(paste0("org\\.",organism$Gs),row.names(installed.packages()),  value=TRUE))
+  if (length(organism$org)!=1) {
+    stop(paste("Can't find annotation for", organism$genus, organism$species))
+  }
+} else {
+  organism <- list(org=args$org_package)
+}
+metadata(dds)$organism <- organism
+metadata(dds)$template_git <- packageDescription("DESdemonA")$git_last_commit
+
+library(organism$org, character.only=TRUE)
+
+mcols(dds)$symbol <- mapIds(
+  eval(parse(text = organism$org)), 
+  keys=row.names(dds),
+  column="SYMBOL",
+  keytype="ENSEMBL",
+  multiVals="first")[row.names(dds)]
+
+mcols(dds)$entrez <- mapIds(
+  eval(parse(text = organism$org)),
+  keys=row.names(dds),
+  column="ENTREZID",
+  keytype="ENSEMBL",
+  multiVals="first")[row.names(dds)]
+
+save(dds, file=file.path("data",paste0("dds", args$spec_suffix, ".rda")))
+saveRDS(dds, file=file.path("data",paste0("dds", args$spec_suffix, ".rds")))
