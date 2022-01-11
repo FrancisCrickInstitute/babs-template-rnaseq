a2ebefd7f6c6291a0e840df498687fb84170691a
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-01-11 20:20:59 +0000

    Refactor so there's an ingress module

--- babs/config/global.mk
+++ babs/config/global.mk
@@ -1,2 +1,2 @@
 label_column := sample # which column from the ASF csv to use in nf-core.  Maybe sample_name
-global_docs_dir := ../babs
+nf_cache_dir := /camp/apps/misc/stp/babs/nf-core/singularity/rnaseq/3.4/
