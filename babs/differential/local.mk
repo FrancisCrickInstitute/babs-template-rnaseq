15051272501d2b0ee8120a74b00d1cf80f0110c0
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-01-12 13:15:20 +0000

    Improve bootstrap/exec separation

---
+++ babs/differential/local.mk
@@ -0,0 +1,13 @@
+R := module load pandoc/2.2.3.2-foss-2016b;  module load R/4.0.3-foss-2020a; R 
+
+# which metadata column will match ASF's 'sample' column.
+meta2asf := ASF_lims_id
+# Where to look for experimental design information
+name_column  := "name","label","sample","filename"
+# Produce a self-contained report (false to link images etc)
+contained    := FALSE
+
+
+# Git variables
+TAG = $(shell git describe --tags --dirty=_altered --always --long)# e.g. v1.0.2-2-ace1729a
+VERSION = $(shell git describe --tags --abbrev=0)#e.g. v1.0.2
