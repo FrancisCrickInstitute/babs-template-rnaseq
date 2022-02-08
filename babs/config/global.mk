7667e49cce1205888697342cd3589df904207ac6
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-02-08 10:02:53 +0000

    Fix #5 Add babs id onto nfcore working path
    Also create a link nf_core  in the nfcore directory to the working folder.

--- babs/config/global.mk
+++ babs/config/global.mk
@@ -1,2 +1,3 @@
 label_column := sample # which column from the ASF csv to use in nf-core.  Maybe sample_name
 SQLITE=ml SQLite/3.36-GCCcore-11.2.0; sqlite3
+babsid=$(shell sed -n  "s/ *Hash: *//p" ../../.babs)
--- babs/nfcore/local.mk
+++ babs/nfcore/local.mk
@@ -1,2 +1,2 @@
-nf_work = /camp/stp/babs/scratch/bioinformatics/projects/
+nf_work = /camp/stp/babs/scratch/bioinformatics/projects/${babsid}/
 nf_cache_dir = /camp/apps/misc/stp/babs/nf-core/singularity/rnaseq/3.4/
