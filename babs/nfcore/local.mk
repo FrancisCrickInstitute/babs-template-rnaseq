f765edd55998a20e2ead4456ff6762dfa9aac8de
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-02-08 10:16:24 +0000

    Add extra info to scratch space.

--- babs/nfcore/local.mk
+++ babs/nfcore/local.mk
@@ -1,2 +1,2 @@
-nf_work = /camp/stp/babs/scratch/bioinformatics/projects/${babsid}/
+nf_work = /camp/stp/babs/scratch/bioinformatics/projects/${babsid}/nfcore/
 nf_cache_dir = /camp/apps/misc/stp/babs/nf-core/singularity/rnaseq/3.4/
