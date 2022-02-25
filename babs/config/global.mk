3e6dc4e084bd19c081dc358ba31e044dce202075
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-02-25 17:15:08 +0000

    Fix some typos

--- babs/config/global.mk
+++ babs/config/global.mk
@@ -1,3 +1,6 @@
 label_column := sample # which column from the ASF csv to use in nf-core.  Maybe sample_name
 SQLITE=ml SQLite/3.36-GCCcore-11.2.0; sqlite3
 babsid=$(shell sed -n  "s/ *Hash: *//p" ../../.babs)
+
+#Debugging tool - `make print-varname` will show variable's value
+print-%: ; @echo $*=$($*)
