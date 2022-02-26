232c06a12172cadf0a43e81e6059ab200a572bfe
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-02-26 13:44:51 +0000

    Rationalise how settings are used within differential module

--- babs/config/global.mk
+++ babs/config/global.mk
@@ -1,6 +1,7 @@
 label_column := sample # which column from the ASF csv to use in nf-core.  Maybe sample_name
 SQLITE=ml SQLite/3.36-GCCcore-11.2.0; sqlite3
 babsid=$(shell sed -n  "s/ *Hash: *//p" ../../.babs)
+babsproject=$(shell sed -n  "s/ *Project: *//p" ../../.babs)
 
 #Debugging tool - `make print-varname` will show variable's value
 print-%: ; @echo $*=$($*)
--- babs/differential/local.mk
+++ babs/differential/local.mk
@@ -1,12 +1,16 @@
 R := module load pandoc/2.2.3.2-foss-2016b;  module load R/4.0.3-foss-2020a; command R 
 
+# What's BABS' experimental table (excluding csv file extention)
+BABS_TABLE := experiment_table
+
 # which metadata column will match ASF's 'sample' column.
-	meta2asf := ID
+meta2asf := ID
+
 # Where to look for experimental design information
-name_column  := "name","label","sample","filename"
+NAME_COLUMN  := "name","label","sample","filename"
+
 # Produce a self-contained report (false to link images etc)
 contained    := FALSE
-
 
 # Git variables
 TAG = $(shell git describe --tags --dirty=_altered --always --long)# e.g. v1.0.2-2-ace1729a
