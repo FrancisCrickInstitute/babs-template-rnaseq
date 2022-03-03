ccacf7cecb5d9fc7a0a798b58aba88346f5cb857
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-03-03 16:39:33 +0000

    Fixing some expanded/unexpanded macro confusion
    For some reason I suddenly thought := was the 'expand-macro' version
    of assignment, so there was a smattering of incorrect corrections a
    version or two ago.  This commit puts all(?) of these back to the
    correct way round.

--- babs/differential/local.mk
+++ babs/differential/local.mk
@@ -7,11 +7,11 @@ BABS_TABLE = experiment_table
 meta2asf = ID
 
 # Where to look for experimental design information
-NAME_COLUMN  := "sample_label","name","label","sample","filename","$(meta2asf)"
+NAME_COLUMN  = "sample_label","name","label","sample","filename","$(meta2asf)"
 
 # Produce a self-contained report (false to link images etc)
 contained    = FALSE
 
 # Git variables
-TAG := $(shell git describe --tags --dirty=_altered --always --long)# e.g. v1.0.2-2-ace1729a
-VERSION := $(shell git describe --tags --abbrev=0)#e.g. v1.0.2
+TAG = $(shell git describe --tags --dirty=_altered --always --long)# e.g. v1.0.2-2-ace1729a
+VERSION = $(shell git describe --tags --abbrev=0)#e.g. v1.0.2
