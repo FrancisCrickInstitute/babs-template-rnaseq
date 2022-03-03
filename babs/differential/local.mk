9375cf9d8433a7f686c7186f50b1c4eb6423db81
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-03-03 13:57:46 +0000

    Add a module to download public datasets

--- babs/differential/local.mk
+++ babs/differential/local.mk
@@ -1,17 +1,17 @@
-R := module load pandoc/2.2.3.2-foss-2016b;  module load R/4.0.3-foss-2020a; command R 
+R = module load pandoc/2.2.3.2-foss-2016b;  module load R/4.0.3-foss-2020a; command R 
 
 # What's BABS' experiment table (excluding csv file extention)
-BABS_TABLE := experiment_table
+BABS_TABLE = experiment_table
 
 # which metadata column will match ASF's 'sample' column.
-meta2asf := ID
+meta2asf = ID
 
 # Where to look for experimental design information
 NAME_COLUMN  := "sample_label","name","label","sample","filename","$(meta2asf)"
 
 # Produce a self-contained report (false to link images etc)
-contained    := FALSE
+contained    = FALSE
 
 # Git variables
-TAG = $(shell git describe --tags --dirty=_altered --always --long)# e.g. v1.0.2-2-ace1729a
-VERSION = $(shell git describe --tags --abbrev=0)#e.g. v1.0.2
+TAG := $(shell git describe --tags --dirty=_altered --always --long)# e.g. v1.0.2-2-ace1729a
+VERSION := $(shell git describe --tags --abbrev=0)#e.g. v1.0.2
---
+++ babs/fetch_ngs/local.mk
@@ -0,0 +1,3 @@
+nf_work = /camp/stp/babs/scratch/bioinformatics/projects/${babsid}/nfcore/
+nf_cache_dir = /camp/apps/misc/stp/babs/nf-core/singularity/fetchngs/1.5/
+fetchngs_sample_column=sample_title
