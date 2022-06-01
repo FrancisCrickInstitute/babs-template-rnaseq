960e4a7a5af2ce0b8d8ddd5dc2e56344073a9f52
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-06-01 14:01:40 +0100

    Fine tuning makefile

--- babs/config/global.mk
+++ babs/config/global.mk
@@ -6,7 +6,7 @@ babsproject=$(shell sed -n  "s/ *Project: *//p" ../../.babs)
 
 #Executibles (can be overridden in local.mk's)
 NEXTFLOW := ml purge; ml Nextflow/21.10.3; ml Singularity/3.4.2; ml CAMP_proxy; nextflow
-R := module load pandoc/2.2.3.2-foss-2016b;  module load R/4.0.3-foss-2020a; command R 
+R := module load pandoc/2.2.3.2-foss-2016b;  module load R/4.0.3-foss-2020a; command R
 SQLITE := ml SQLite/3.36-GCCcore-11.2.0; sqlite3
 
 #Debugging tool - `make print-varname` will show variable's value
@@ -20,5 +20,5 @@ comma:= ,
 empty:=
 space:= $(empty) $(empty)
 
-help: ## show help message
+help: ## Show help message
 	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m\033[0m\n"} /^[$$()% 0-9a-zA-Z_-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
