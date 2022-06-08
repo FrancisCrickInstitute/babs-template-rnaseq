a26a2c2113908d02cb7af268397eccb8050072ae
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-06-08 13:21:13 +0100

    Improve renv handling

--- babs/differential/local.mk
+++ babs/differential/local.mk
@@ -1,4 +1,6 @@
-R = module load pandoc/2.2.3.2-foss-2016b;  module load R/4.1.2-foss-2021b; ml NLopt/2.7.0-GCCcore-11.2.0 ; command R 
+Rversion = 4.0.3-foss-2020b
+R = module load pandoc/2.2.3.2-foss-2016b  R/$(Rversion) NLopt/2.7.0-GCCcore-11.2.0 OpenBLAS/0.3.18-GCC-11.2.0 zstd/1.4.5-GCCcore-10.2.0; command R
+
 
 my_counts_dir=inst/extdata/genes.results
 nfcore_dir=../nfcore/results
@@ -17,3 +19,14 @@ contained    = FALSE
 # Git variables
 TAG = $(shell git describe --tags --dirty=_altered --always --long)# e.g. v1.0.2-2-ace1729a
 VERSION = $(shell git describe --tags --abbrev=0)#e.g. v1.0.2
+
+define slurm
+#! /usr/bin/bash
+#SBATCH --partition=cpu
+#SBATCH --time='0-02:00:00'
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --job-name=$(notdir $(CURDIR))
+#SBATCH --output=slurm-%x-%A_%a.out
+endef
+export slurm
