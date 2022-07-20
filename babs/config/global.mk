dc603d2ce845c76b2c27985e6bf82cd8835fb2d0
Gavin Kelly <gavin.kelly@crick.ac.uk>
2022-07-20 17:08:36 +0100

    Add updaters for modules, pipeline and github-issues handling

--- babs/config/global.mk
+++ babs/config/global.mk
@@ -27,3 +27,8 @@ space:= $(empty) $(empty)
 
 help: ## Show help message
 	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m\033[0m\n"} /^[$$()% 0-9a-zA-Z_-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
+
+update-module: ## Update the specific module you're currently using.
+	git stash -m "Stashing state prior to module update"
+	cp -r --no-preserve=mode /camp/stp/babs/working/bioinformatics/templates/rnaseq/babs/$(notdir $(CURDIR))/. .
+
