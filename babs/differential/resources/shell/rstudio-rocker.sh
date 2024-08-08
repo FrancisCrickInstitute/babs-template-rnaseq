b1b012c5360c9fd28c5cd6e9d7e467604e5cd3b3
Gavin Kelly <gavin.kelly@crick.ac.uk>
2024-08-08 10:15:31 +0100

    Fix the rstudio launcher

--- babs/differential/resources/shell/rstudio-rocker.sh
+++ babs/differential/resources/shell/rstudio-rocker.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
-#SBATCH --output=rstudio-server.job.log
+#SBATCH --output=rstudio-server.log
 #SBATCH --job-name=rstudio
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=12
-#SBATCH --partition=cpu
-#SBATCH --time='8:00:00'
-#SBATCH --mem=64G
 
-ml Singularity/3.6.4 
+ml Singularity/${2:-3.11.3}
 
 # Create temporary directory to be populated with directories to bind-mount in the container
 # where writable file systems are necessary. Adjust path as appropriate for your computing environment.
-IMAGE=${IMAGE:-/flask/apps/containers/all-singularity-images/verse-boost_4.2.2.sif}
 workdir=$(mktemp -u)
 
 mkdir -p -m 700 ${workdir}/run ${workdir}/tmp ${workdir}/var/lib/rstudio-server ${workdir}/rstudio ${workdir}/R
@@ -46,14 +41,15 @@ cat > ${workdir}/rstudio/rstudio-prefs.json <<EOF
 EOF
 
 chmod +x ${workdir}/rsession.sh
-
+set -a
+. ./rocker.Renviron
+set +a
 
 export SINGULARITY_BIND="${workdir}/run:/run, ${workdir}/tmp:/tmp, ${workdir}/database.conf:/etc/rstudio/database.conf, \
                          ${workdir}/rsession.sh:/etc/rstudio/rsession.sh, ${workdir}/var/lib/rstudio-server:/var/lib/rstudio-server, \
 			 ${workdir}/rsession.conf:/etc/rstudio/rsession.conf, ${workdir}/R:$HOME/.config/R, ${workdir}/rstudio:$HOME/.config/rstudio, \
                          ${PWD}/rocker.Renviron:/usr/local/lib/R/etc/Renviron.site, /etc/ssl/certs/ca-bundle.crt, ${workdir}/rserver.sh, \
-                         $HOME/.ssh,/sys/fs/cgroup, \
-                         /nemo/project/apps,/nemo/svc,/nemo/stp,/camp/stp/,/nemo/lab"
+                         $HOME/.ssh,/sys/fs/cgroup,${PWD},${RENV_PATHS_ROOT}"
 
 # Do not suspend idle sessions.
 # Alternative to setting session-timeout-minutes=0 in /etc/rstudio/rsession.conf
@@ -65,53 +61,57 @@ export SINGULARITYENV_PASSWORD=$(openssl rand -base64 15)
 # get unused socket per https://unix.stackexchange.com/a/132524
 # tiny race condition between the python & singularity commands
 readonly PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+
+## Produce the informative message
+
+if [ -z "$3" ]; then
+    echo "1. Point your web browser to http://$(hostname).$(dnsdomainname):${PORT}"
+else
 cat 1>&2 <<END
-1. Point your web browser to http://$(hostname).camp.thecrick.org:${PORT}
-
-2. SSH tunnel from your workstation using the following command:
-
-   ssh -N -L 8787:$(hostname).camp.thecrick.org:${PORT} ${SINGULARITYENV_USER}@LOGIN-HOST
-
+1. SSH tunnel from your workstation using the following command:
+   ssh -A -N -L 8787:$(hostname).$(dnsdomainname):${PORT} ${SINGULARITYENV_USER}@$3.$(dnsdomainname)
    and point your web browser to http://localhost:8787
-
-3. log in to RStudio Server using the following credentials:
+END
+fi
+cat 1>&2 <<END
+2. Log in to RStudio Server using the following credentials:
 
    user: ${SINGULARITYENV_USER}
    password: ${SINGULARITYENV_PASSWORD}
 
-When done using RStudio Server, terminate the job by:
+When done using RStudio Server, terminate the job:
 
 1. Exit the RStudio Session ("power" button in the top right corner of the RStudio window)
+END
+if [ -z "$3" ]; then
+echo "2. Quit (Ctrl+C) this process"
+else
+cat 1>&2 <<END
 2. Issue the following command on the login node:
-
       scancel -f ${SLURM_JOB_ID}
 END
+fi
 
-# We will write Rserver script in a file to send notifications
-cat > ${workdir}/rserver.sh <<END
-#!/bin/sh
+## Generate the script that will run RStudio
 
-# Notification:
-[ "${NTFY}" == "" ] || \
-curl -s -H "Title: RStudio server ready" \
-     -H "Tag:information_source" \
-     -d "Access server at:
-            http://$(hostname).camp.thecrick.org:${PORT}
+echo "#!/bin/sh" > ${workdir}/rserver.sh
 
-        To cancel job, if on SLURM queue:
-          scancel -f ${SLURM_JOB_ID}" \
-        https://ntfy.sh/${NTFY} &> /dev/null
+if [ ! -z "$3" ] && [ ! -z "${NTFY}"]; then
+cat >> ${workdir}/rserver.sh <<END
+curl -s -H "Title: RStudio server ready" -H "Tag:information_source"  https://ntfy.sh/${NTFY} \
+ -d "Access server on port ${PORT} - see rstudio-server.log for details. To cancel any slurm job, scancel -f ${SLURM_JOB_ID}"  >/dev/null 2>&1
+END
+fi
 
-# Start server
+cat >> ${workdir}/rserver.sh <<END
 /usr/lib/rstudio-server/bin/rserver --www-port ${PORT} \
     --auth-none=1 \
     --auth-pam-helper-path=pam-helper \
     --server-user $(whoami) \
     --auth-stay-signed-in-days=30 \
     --auth-timeout-minutes=0 \
-    --rsession-path=/etc/rstudio/rsession.sh
-
-printf 'rserver exited' 1>&2
+    --rsession-path=/etc/rstudio/rsession.sh  
 END
 
-singularity exec --cleanenv --containall ${IMAGE} sh ${workdir}/rserver.sh
+## Run the above generated script within singularity
+singularity exec --cleanenv --containall $1 sh ${workdir}/rserver.sh >/dev/null 2>&1
